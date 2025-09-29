b2aea8fdd11f0ae00bcb779982376e439384f41e
nishuldhakar <nishuldhakar.se@gmail.com>
2025-09-29 12:22:30 +0530

    initial

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,23 @@
 
+import React from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+
+import Createusers from './components/Createusers'
+import ListUsers from './components/ListUsers'
+
 function App() {
 
   return (
-    <div className="text-center text-2xl font-bold bg-amber-800">
-        Hello world
-    </div>
+    <BrowserRouter>
+<nav className="bg-amber-300 p-4 text-center text-blue-600 font-semibold">
+  <Link to="/">List Users</Link> | <Link to="/user/create">Create User</Link>
+</nav>
+<Routes>
+  <Route index element={<ListUsers />} />
+  <Route path="user/create" element={<Createusers />} />
+</Routes>
+
+    </BrowserRouter>
   )
 }
 
